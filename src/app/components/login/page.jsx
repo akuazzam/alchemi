b2489414de9572aa4203737f32304a67965285ef
Hamede <hamede.abdulgafur@mnsu.edu>
2023-11-22 17:49:17 -0600

    Login page design

--- src/app/components/login/page.jsx
+++ src/app/components/login/page.jsx
@@ -1,6 +1,16 @@
+import styles from "@/app/components/Ui/login/login.module.css"
+
 const LoginPage = ()=>{
     return (
-        <div>LoginPage</div>
+        <div className={styles.container}>
+            
+            <form action="" className={styles.form}>
+                <h1>Login</h1>
+                <input type="email" placeholder="Email"/>
+                <input type="pasword" placeholder="Password"/>
+                <button>Login</button>
+            </form>
+        </div>
     )
 }
 export default LoginPage 

55a46b4d921480508c7dcb791bf6ad6a9c2573b1
Melan Shifa <101609150+MelanShifa@users.noreply.github.com>
2023-11-22 22:30:36 -0600

    loginSignup

--- src/app/components/login/signup/page.jsx
+++ src/app/components/login/signup/page.jsx
@@ -1,19 +1,57 @@
-// Adjust the import statement to match your project structure
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from "@/app/components/Ui/login/signup/signup.module.css";
-import React from 'react';
- 
+
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch('/api/signUp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // You might want to redirect the user to the login page or confirm the sign-up
+        router.push('/components/dashboard'); // Update with your actual route to the login page
+      } else {
+        // Handle errors, e.g. display an error message
+        console.error('Sign up failed');
+      }
+    } catch (error) {
+      console.error('An error occurred during sign up:', error);
+    }
+  };
+
   return (
-<div className={styles.container}>
-<form action="" className={styles.form}>
-<h1>Sign Up</h1>
-<input type="email" placeholder="Email" />
-<input type="password" placeholder="Create Password" />
-        {/* You might want to add more fields for the sign-up form */}
-<button>Sign Up</button>
-</form>
-</div>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h1>Sign Up</h1>
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder="Create Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 };
- 
+
 export default SignUp;

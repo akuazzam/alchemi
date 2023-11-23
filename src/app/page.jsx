d350f0d0f291357abf5c15e71deb7eff39c31ed9
NK2121 <nahomnk11@gmail.com>
2023-11-22 20:05:30 -0600

    nav_bar_update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { signInWithGoogle } from "./firebase/auth"
 import Dashboard from "./components/dashboard/page"
 import LoginPage from "./components/login/page"
+import Navbar from './components/Ui/dashboard/navbar/navbar';
 
 const Home = () => {
   return (
     <div>
-      {<button onClick={signInWithGoogle}>Sign in with Google</button>}
+      {/* {<button onClick={signInWithGoogle}>Sign in with Google</button>} */}
       HomePage
+      <Navbar/>
     </div>
   );
 };

f64e2858a736bf95ee6f8f2e75eefa231b1ef5d6
Hamede <hamede.abdulgafur@mnsu.edu>
2023-11-23 02:37:30 -0600

    Profile link to dashboard

---
+++ src/app/components/login/signup/profile/page.jsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from "@/app/components/Ui/login/signup/profile/profile.module.css";
+import Dashboard from '@/app/components/dashboard/page';
+
+const ProfileCreation = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    major: '',
+    school: ''
+  });
+
+  // State to hold the router object
+  const router = useRouter();
+
+  // useEffect to ensure useRouter is called in the client-side environment
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Handle profile creation logic here, e.g., send data to your API
+
+    // Redirect to the dashboard after successful profile creation
+    // Check if router is available (which indicates we are client-side)
+    if (router) {
+      router.push('/components/dashboard'); // Make sure '/dashboard' is the correct path to your dashboard
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h1>Create Your Profile</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="major"
+          placeholder="Major"
+          value={formData.major}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="school"
+          placeholder="School"
+          value={formData.school}
+          onChange={handleChange}
+        />
+        <button type="submit">Complete Profile</button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileCreation;

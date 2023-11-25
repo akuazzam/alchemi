d3726803de2a118c38c7de8f0effc1c68fca70bf
Hamede <hamede.abdulgafur@mnsu.edu>
2023-11-25 05:40:58 -0600

    home page made to the first page to show up

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import Dashboard from "./components/dashboard/page"
 import Navbar from './components/Ui/dashboard/navbar/navbar';
 import LoginPage from './components/login/page';
+import HomePage from './components/home/page';
 
 const Home = () => {
   return (
-  <LoginPage/>
+  <HomePage/>
   );
 };
 

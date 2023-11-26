f247e1e6d2e9fbda471569b83172a572dc8c0c0b
melanshifa <melan.shifa@mnsu.edu>
2023-11-26 14:18:21 -0600

    fix

--- pages/api/getUserCourses.js
+++ pages/api/getUserCourses.js
@@ -4,21 +4,34 @@ import * as Realm from 'realm-web';
 import { ObjectId } from 'mongodb'; // Import ObjectId
 
 export default async function handler(req, res) {
+  // Extract token from the Authorization header
+  const authHeader = req.headers.authorization || '';
+  const token = authHeader.startsWith('Bearer ') ? authHeader.substring(7) : null;
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  // Initialize the Realm app
   const app = new Realm.App({ id: process.env.NEXT_PUBLIC_REALM_APP_ID });
-  const user = app.currentUser;
 
-  if (!user) {
-    return res.status(401).json({ error: 'Unauthorized' });
+  try {
+    // Verify the token
+    const accessToken = Realm.Credentials.jwt(token);
+    const user = await app.logIn(accessToken);
+    
+
+    if (!user) {
+      throw new Error('Invalid token');
+    }
+
+    const userId = user.id;
+    const { db } = await connectToDatabase();
+    const objectId = new ObjectId(userId);
+
+    const courses = await db.collection('courses').find({ createdBy: objectId }).toArray();
+    res.status(200).json(courses);
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized: ' + error.message });
   }
-  const userId = user.id;
-  if (!userId) {
-    return res.status(400).json({ error: 'UserId is required' });
-  }
-
-  const { db } = await connectToDatabase();
-  const objectId = new ObjectId(userId);
-
-  const courses = await db.collection('courses').find({ createdBy: objectId }).toArray();
-
-  res.status(200).json(courses);
 }
